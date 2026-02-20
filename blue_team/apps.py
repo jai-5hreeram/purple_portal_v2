9277ff19cd1217ad84b7e78ba5cdf198b596069a
jai-5hreeram <ashwin.k7118@gmail.com>
2026-02-20 12:40:01 +0530

    Restarting: Clean Django structure

---
+++ blue_team/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlueTeamConfig(AppConfig):
+    name = 'blue_team'
---
+++ red_team/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RedTeamConfig(AppConfig):
+    name = 'red_team'
